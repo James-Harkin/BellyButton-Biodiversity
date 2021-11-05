45bde12961a3c5038d5b2331c6e132e06bd43b81
James-Harkin <jamesrharkin@gmail.com>
2021-11-05 00:33:22 -0500

    fixing file paht

--- js/charts.js
+++ js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("..data/samples.json").then((data) => {
+  d3.json("js/data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("..data/samples.json").then((data) => {
+  d3.json("js/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +56,7 @@ function buildMetadata(sample) {
   //1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("..data/samples.json").then((data) => {
+  d3.json("js/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var chartData = data.samples
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -101,7 +101,7 @@ function buildCharts(sample) {
 // Create the buildCharts function.
 function buildChartsBubble(sample) {
   // Use d3.json to load and retrieve the samples.json file 
-  d3.json("..data/samples.json").then((data) => {
+  d3.json("js/data/samples.json").then((data) => {
     var chartData = data.samples
     var resultArray = chartData.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
@@ -141,7 +141,7 @@ function buildChartsBubble(sample) {
 // Create the buildChart function.
 function buildChartsGauge(sample) {
   // Use d3.json to load the samples.json file 
-  d3.json("..data/samples.json").then((data) => {
+  d3.json("js/data/samples.json").then((data) => {
     console.log(data);
 
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
